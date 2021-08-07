9386216cc044d42f18109954984768eb20266323
Simon <simme.jakobsson@gmail.com>
2021-08-07 20:41:53 +0200

    carrots

---
+++ carrots.cpp
@@ -0,0 +1,18 @@
+//
+// Created by simon on 2021-08-07.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    string c;
+    cin >> a >> b;
+    cin;
+    for (int i =0; i <a; i++){
+        cin;
+    }
+    cout << b << endl;
+}
+
