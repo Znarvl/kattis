7a56981338ad6e1ba6c37e92bee74d841106d1ad
Simon <simme.jakobsson@gmail.com>
2021-08-07 20:24:22 +0200

    added c++

---
+++ faktor.cpp
@@ -0,0 +1,14 @@
+//
+// Created by simon on 2021-08-07.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin >> a >> b;
+    int ans = a * (b-1);
+    cout << ans + 1 << endl;
+
+}
